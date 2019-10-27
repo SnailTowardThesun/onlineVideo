33a536ce58d2899ca2d69c22f0103a534bb7765f
SnailTowardThesun <hanvskun@hotmail.com>
2019-10-27 23:51:32 +0800

    channel page

--- web/onlineVideo/src/app/channel/channel.component.ts
+++ web/onlineVideo/src/app/channel/channel.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChannelComponent implements OnInit {
 
+  subChannel = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11'
+  ];
+
   constructor() { }
 
   ngOnInit() {
