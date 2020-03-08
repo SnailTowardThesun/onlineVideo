55eaee5d1aaf0c3b06cf2f5818cde7a786756e39
ME_Kun_Han <hanvskun@hotmail.com>
2020-03-08 23:03:25 +0800

    add nvigate part

--- web/onlineVideo/src/app/app.module.ts
+++ web/onlineVideo/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { IndexComponent } from './index/index.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material-module';
+import { NavigateComponent } from './navigate/navigate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChannelComponent,
-    IndexComponent
+    IndexComponent,
+    NavigateComponent
   ],
   imports: [
     BrowserModule,
